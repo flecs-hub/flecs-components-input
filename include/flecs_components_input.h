0af3581bdb54336c8b9edc9675a42b9083aee7de
Sander Mertens <sander.mertens8@gmail.com>
2020-09-16 19:27:36 -0700

    Add C++ wrapper

--- include/flecs_components_input.h
+++ include/flecs_components_input.h
@@ -8,85 +8,85 @@
 extern "C" {
 #endif
 
-#define ECS_KEY_UNKNOWN (0)
-#define ECS_KEY_RETURN ('\r')
-#define ECS_KEY_ESCAPE ('\033')
-#define ECS_KEY_BACKSPACE ('\b')
-#define ECS_KEY_TAB ('\t')
-#define ECS_KEY_SPACE (' ')
-#define ECS_KEY_EXCLAIM ('!')
-#define ECS_KEY_QUOTEDBL ('"')
-#define ECS_KEY_HASH ('#')
-#define ECS_KEY_PERCENT ('%')
-#define ECS_KEY_DOLLAR ('$')
-#define ECS_KEY_AMPERSAND ('&')
-#define ECS_KEY_QUOTE ('\'')
-#define ECS_KEY_LEFTPAREN ('(')
-#define ECS_KEY_RIGHTPAREN (')')
-#define ECS_KEY_ASTERISK ('*')
-#define ECS_KEY_PLUS ('+')
-#define ECS_KEY_COMMA (',')
-#define ECS_KEY_MINUS ('-')
-#define ECS_KEY_PERIOD ('.')
-#define ECS_KEY_SLASH ('/')
-#define ECS_KEY_0 ('0')
-#define ECS_KEY_1 ('1')
-#define ECS_KEY_2 ('2')
-#define ECS_KEY_3 ('3')
-#define ECS_KEY_4 ('4')
-#define ECS_KEY_5 ('5')
-#define ECS_KEY_6 ('6')
-#define ECS_KEY_7 ('7')
-#define ECS_KEY_8 ('8')
-#define ECS_KEY_9 ('9')
-#define ECS_KEY_COLON (':')
-#define ECS_KEY_SEMICOLON (';')
-#define ECS_KEY_LESS ('<')
-#define ECS_KEY_EQUALS ('=')
-#define ECS_KEY_GREATER ('>')
-#define ECS_KEY_QUESTION ('?')
-#define ECS_KEY_AT ('@')
-#define ECS_KEY_LEFTBRACKET ('[')
-#define ECS_KEY_BACKSLASH ('\\')
-#define ECS_KEY_RIGHTBRACKET (']')
-#define ECS_KEY_CARET ('^')
-#define ECS_KEY_UNDERSCORE ('_')
-#define ECS_KEY_BACKQUOTE ('`')
-#define ECS_KEY_A ('a')
-#define ECS_KEY_B ('b')
-#define ECS_KEY_C ('c')
-#define ECS_KEY_D ('d')
-#define ECS_KEY_E ('e')
-#define ECS_KEY_F ('f')
-#define ECS_KEY_G ('g')
-#define ECS_KEY_H ('h')
-#define ECS_KEY_I ('i')
-#define ECS_KEY_J ('j')
-#define ECS_KEY_K ('k')
-#define ECS_KEY_L ('l')
-#define ECS_KEY_M ('m')
-#define ECS_KEY_N ('n')
-#define ECS_KEY_O ('o')
-#define ECS_KEY_P ('p')
-#define ECS_KEY_Q ('q')
-#define ECS_KEY_R ('r')
-#define ECS_KEY_S ('s')
-#define ECS_KEY_T ('t')
-#define ECS_KEY_U ('u')
-#define ECS_KEY_V ('v')
-#define ECS_KEY_W ('w')
-#define ECS_KEY_X ('x')
-#define ECS_KEY_Y ('y')
-#define ECS_KEY_Z ('z')
-#define ECS_KEY_DELETE (127)
+#define ECS_KEY_UNKNOWN ((int)0)
+#define ECS_KEY_RETURN ((int)'\r')
+#define ECS_KEY_ESCAPE ((int)'\033')
+#define ECS_KEY_BACKSPACE ((int)'\b')
+#define ECS_KEY_TAB ((int)'\t')
+#define ECS_KEY_SPACE ((int)' ')
+#define ECS_KEY_EXCLAIM ((int)'!')
+#define ECS_KEY_QUOTEDBL ((int)'"')
+#define ECS_KEY_HASH ((int)'#')
+#define ECS_KEY_PERCENT ((int)'%')
+#define ECS_KEY_DOLLAR ((int)'$')
+#define ECS_KEY_AMPERSAND ((int)'&')
+#define ECS_KEY_QUOTE ((int)'\'')
+#define ECS_KEY_LEFTPAREN ((int)'(')
+#define ECS_KEY_RIGHTPAREN ((int)')')
+#define ECS_KEY_ASTERISK ((int)'*')
+#define ECS_KEY_PLUS ((int)'+')
+#define ECS_KEY_COMMA ((int)',')
+#define ECS_KEY_MINUS ((int)'-')
+#define ECS_KEY_PERIOD ((int)'.')
+#define ECS_KEY_SLASH ((int)'/')
+#define ECS_KEY_0 ((int)'0')
+#define ECS_KEY_1 ((int)'1')
+#define ECS_KEY_2 ((int)'2')
+#define ECS_KEY_3 ((int)'3')
+#define ECS_KEY_4 ((int)'4')
+#define ECS_KEY_5 ((int)'5')
+#define ECS_KEY_6 ((int)'6')
+#define ECS_KEY_7 ((int)'7')
+#define ECS_KEY_8 ((int)'8')
+#define ECS_KEY_9 ((int)'9')
+#define ECS_KEY_COLON ((int)':')
+#define ECS_KEY_SEMICOLON ((int)';')
+#define ECS_KEY_LESS ((int)'<')
+#define ECS_KEY_EQUALS ((int)'=')
+#define ECS_KEY_GREATER ((int)'>')
+#define ECS_KEY_QUESTION ((int)'?')
+#define ECS_KEY_AT ((int)'@')
+#define ECS_KEY_LEFTBRACKET ((int)'[')
+#define ECS_KEY_BACKSLASH ((int)'\\')
+#define ECS_KEY_RIGHTBRACKET ((int)']')
+#define ECS_KEY_CARET ((int)'^')
+#define ECS_KEY_UNDERSCORE ((int)'_')
+#define ECS_KEY_BACKQUOTE ((int)'`')
+#define ECS_KEY_A ((int)'a')
+#define ECS_KEY_B ((int)'b')
+#define ECS_KEY_C ((int)'c')
+#define ECS_KEY_D ((int)'d')
+#define ECS_KEY_E ((int)'e')
+#define ECS_KEY_F ((int)'f')
+#define ECS_KEY_G ((int)'g')
+#define ECS_KEY_H ((int)'h')
+#define ECS_KEY_I ((int)'i')
+#define ECS_KEY_J ((int)'j')
+#define ECS_KEY_K ((int)'k')
+#define ECS_KEY_L ((int)'l')
+#define ECS_KEY_M ((int)'m')
+#define ECS_KEY_N ((int)'n')
+#define ECS_KEY_O ((int)'o')
+#define ECS_KEY_P ((int)'p')
+#define ECS_KEY_Q ((int)'q')
+#define ECS_KEY_R ((int)'r')
+#define ECS_KEY_S ((int)'s')
+#define ECS_KEY_T ((int)'t')
+#define ECS_KEY_U ((int)'u')
+#define ECS_KEY_V ((int)'v')
+#define ECS_KEY_W ((int)'w')
+#define ECS_KEY_X ((int)'x')
+#define ECS_KEY_Y ((int)'y')
+#define ECS_KEY_Z ((int)'z')
+#define ECS_KEY_DELETE ((int)127)
 
-#define ECS_KEY_RIGHT ('R')
-#define ECS_KEY_LEFT ('L')
-#define ECS_KEY_DOWN ('D')
-#define ECS_KEY_UP ('U')
-#define ECS_KEY_CTRL ('C')
-#define ECS_KEY_SHIFT ('S')
-#define ECS_KEY_ALT ('A')
+#define ECS_KEY_RIGHT ((int)'R')
+#define ECS_KEY_LEFT ((int)'L')
+#define ECS_KEY_DOWN ((int)'D')
+#define ECS_KEY_UP ((int)'U')
+#define ECS_KEY_CTRL ((int)'C')
+#define ECS_KEY_SHIFT ((int)'S')
+#define ECS_KEY_ALT ((int)'A')
 
 ECS_STRUCT(ecs_key_state_t, {
     bool pressed;
@@ -126,6 +126,29 @@ void FlecsComponentsInputImport(
 
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef __cplusplus
+
+namespace flecs {
+namespace components {
+
+class input : FlecsComponentsInput {
+public:
+    using Input = EcsInput;
+
+    input(flecs::world& ecs) {
+        FlecsComponentsInputImport(ecs.c_ptr());
+
+        ecs.module<flecs::components::input>();
+
+        ecs.pod_component<Input>("flecs::components::input::Input");
+    }
+};
+
+}
+}
+
 #endif
 
 #endif
